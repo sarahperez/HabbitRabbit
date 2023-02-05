25ad6359504de5796b3d524ae52fffff2dd2bb72
Sarah Perez <perezsarah14@gmail.com>
2023-02-05 16:43:59 -0500

    figuring out routing

--- frontend/habbit-rabbit/src/app/app-routing.module.ts
+++ frontend/habbit-rabbit/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CalendarComponent } from './calendar/calendar.component'; 
 
 const routes: Routes = [];
 
