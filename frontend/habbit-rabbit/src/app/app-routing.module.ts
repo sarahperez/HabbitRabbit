52a5adff48c9ae2e53c3765f076414ad3551f438
Sarah Perez <perezsarah14@gmail.com>
2023-02-05 17:27:23 -0500

    simple routing

--- frontend/habbit-rabbit/src/app/app-routing.module.ts
+++ frontend/habbit-rabbit/src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CalendarComponent } from './calendar/calendar.component'; 
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'calendar', component: CalendarComponent }
+  ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
