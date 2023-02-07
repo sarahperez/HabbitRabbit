42f5aba6ed89e24125c5683fcc1000bed5245869
Sarah Perez <perezsarah14@gmail.com>
2023-02-07 18:33:48 -0500

    added full calendar component

--- frontend/habbit-rabbit/src/app/calendar/calendar.component.ts
+++ frontend/habbit-rabbit/src/app/calendar/calendar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-calendar',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent {
-
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin]
+  };
 }
