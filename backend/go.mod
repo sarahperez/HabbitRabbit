a48cad7627265a30d01f9176e852912835031751
Sophie Ruetschi <Sophie.ruetschi@gmail.com>
2023-02-12 18:27:42 -0500

    trying to set up a testing file for backend

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,4 @@ module main
 
 go 1.19
 
-require github.com/rs/cors v1.8.3 // indirect
+require github.com/rs/cors v1.8.3
---
+++ backend/main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"testing"
+)
+
+//testing reference: https://pkg.go.dev/testing#hdr-Main
+
+//testing outline
+//func TestXxx(*testing.T)
