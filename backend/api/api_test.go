98904a7735691eefee20c6e6876396fd1a34fb0b
Sophie Ruetschi <Sophie.ruetschi@gmail.com>
2023-02-18 12:31:05 -0500

    starting testing

--- backend/api/api_test.go
+++ backend/api/api_test.go
@@ -4,14 +4,19 @@ import (
 	"testing"
 )
 
-func TestApiTest(t *testing.T) {
-	expectedString := "api test is working"
-	actualString := apiTest()
+//curl.exe -v -X GET http://localhost:3000/home-page
 
-	if expectedString != actualString {
-		t.Errorf("Expected String(%s) is not same as"+
-			" actual String (%s)", expectedString, actualString)
-
+func TestUpperCaseHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/home-page", nil)
+	w := httptest.NewRecorder()
+	GoHome(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
 	}
-
+	if string(data) != "ABC" {
+		t.Errorf("expected: Welcome to the home page, get request recieved. Got: %v", string(data))
+	}
 }
