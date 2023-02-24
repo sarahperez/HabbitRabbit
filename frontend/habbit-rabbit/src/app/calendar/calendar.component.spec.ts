f10dd3229fe84435431cb791cf728e6866973dce
Sarah Perez <perezsarah14@gmail.com>
2023-02-24 14:33:33 -0500

    Added Full calendar unit tests

--- frontend/habbit-rabbit/src/app/calendar/calendar.component.spec.ts
+++ frontend/habbit-rabbit/src/app/calendar/calendar.component.spec.ts
@@ -1,27 +1,64 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture,TestBed } from '@angular/core/testing';
 import { Title } from '@angular/platform-browser';
+import { FullCalendarComponent } from '../full-calendar/full-calendar.component';
 import { CalendarComponent } from './calendar.component';
 
 describe('CalendarComponent', () => {
+  let calendar: CalendarComponent;
+  let fixture: ComponentFixture<CalendarComponent>;
+  let events: FullCalendarComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        CalendarComponent
-      ],
+      declarations: [ CalendarComponent ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(CalendarComponent);
+    calendar = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  
+  it('should create the calendar', () => {
+    expect(calendar).toBeTruthy();
+  });
+  
+});
+
+describe('Calendar Toggle Weekends', () => {
+  it('#clicked() should toggle #hasWeekends', () => {
+    const comp = new FullCalendarComponent();
+    expect(comp.hasWeekends)
+      .withContext('checked at first')
+      .toBe(false);
+    comp.clicked();
+    expect(comp.hasWeekends)
+      .withContext('unchecked after click')
+      .toBe(true);
+    comp.clicked();
+    expect(comp.hasWeekends)
+      .withContext('checked after second click')
+      .toBe(false);
   });
 
-  /**
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(CalendarComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('#clicked() should set #message to "has weekends"', () => {
+    const comp = new FullCalendarComponent();
+    expect(comp.message)
+      .withContext('checked at first')
+      .toMatch(/has weekends/i);
+    comp.clicked();
+    expect(comp.message)
+      .withContext('unchecked after clicked')
+      .toMatch(/had no weekends/i);
   });
-  */
 });
+
 
 describe('Calendar Event', () => {
   it('should create a new event', () => {
+    //const title = ${clickInfo.event.title};
+    //expect(title).changes;
+  });
+
+  it('should delete an event', () => {
     //const title = ${clickInfo.event.title};
     //expect(title).changes;
   });
