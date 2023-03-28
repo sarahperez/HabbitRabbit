ee00222ea97db9d2a3afe771d755b5ab1cc5ad0d
Alexa <alexaangelina09@gmail.com>
2023-03-28 14:46:18 -0400

    Basic list interface

--- frontend/habbit-rabbit/src/app/services/login/login.service.ts
+++ frontend/habbit-rabbit/src/app/services/login/login.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { BehaviorSubject, lastValueFrom } from 'rxjs';
+import { BehaviorSubject, lastValueFrom, firstValueFrom } from 'rxjs';
 import { Router } from '@angular/router';
 
 const httpOptions = {
@@ -23,9 +23,9 @@ export class LoginService {
   ) { }
 
   login(Username: string, Password: string): any {
-    lastValueFrom(this.http.post(this.url, { "username": Username, "password": Password })).then((res: any) => {
-      //if (res && res.jwt)
+    lastValueFrom(this.http.post(this.url, { "username": Username, "password": Password })).then(async (res: any) => {
       if (res&&res.jwt) {
+        sessionStorage.setItem('loggedUser', Username);
         sessionStorage.setItem('jwt', res.jwt);
         this.errorSubject.next(null);
         this.router.navigateByUrl('home');
--- frontend/habbit-rabbit/src/app/todolist/todolist.component.ts
+++ frontend/habbit-rabbit/src/app/todolist/todolist.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
+export interface Item {
+  description: string;
+  done: boolean;
+}
+
+export interface SideNavToggle{
+  screenWidth: number;
+  collapsed: boolean;
+}
+
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent {
+  isSideNavCollapsed = false;
+  screenWidth=0;
+  onToggleSideNav(data: SideNavToggle): void{
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
+  }
 
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'placeholder task', done: false }
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
 }
