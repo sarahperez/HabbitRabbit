48d1fbcdebedfddd7ddb4df16b2419de88f276e9
Sarah Perez <perezsarah14@gmail.com>
2023-03-27 23:15:16 -0400

    added side-nav to todolist page

--- frontend/habbit-rabbit/src/app/todolist/todolist.component.ts
+++ frontend/habbit-rabbit/src/app/todolist/todolist.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface SideNavToggle{
+  screenWidth: number;
+  collapsed: boolean;
+}
+
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
   styleUrls: ['./todolist.component.css']
 })
-export class TodolistComponent {
 
+export class TodolistComponent {
+  isSideNavCollapsed = false;
+  screenWidth=0;
+  onToggleSideNav(data: SideNavToggle): void{
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
+  }
 }
