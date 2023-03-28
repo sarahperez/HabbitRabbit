55effc151bf95d97a2f557948df82cec7bb27c02
Sarah Perez <perezsarah14@gmail.com>
2023-03-27 23:15:36 -0400

    Attempting registering user

--- frontend/habbit-rabbit/src/app/register/register.component.ts
+++ frontend/habbit-rabbit/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RegisterService } from './../services/register/register.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  username: any;
+  password: any;
+  name: any;
+  email: any;
 
+  constructor(
+    private registerService: RegisterService, private router: Router
+  ) { }
+
+  onKey(event: any, type: string) {
+    if (type === 'name') {
+      this.name = event.target.value;
+    } else if (type === 'email') {
+      this.email = event.target.value;
+    } else if (type === 'username') {
+      this.username = event.target.value;
+    } else if (type === 'password') {
+      this.password = event.target.value;
+    }
+  }
+
+  onSubmit() {
+    this.registerService
+        .register(this.name, this.email, this.username, this.password);
+  }
 }
