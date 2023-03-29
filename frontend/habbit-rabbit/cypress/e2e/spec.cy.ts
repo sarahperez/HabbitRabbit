ac49230184dbffd56b0543207f3ccb22fc126800
Sarah Perez <perezsarah14@gmail.com>
2023-03-28 20:07:04 -0400

    Login component cypress testing

--- frontend/habbit-rabbit/cypress/e2e/spec.cy.ts
+++ frontend/habbit-rabbit/cypress/e2e/spec.cy.ts
@@ -1,11 +1,13 @@
 describe('Calendar Page Test', () => {
   it('Registers a user', () => {
     cy.visit('http://localhost:4200/')
+    /*
+    cy.get('.username-input').type('sarahperez14');
+    cy.get('.password-input').type('C0tt0nc4ndy!');
 
-    //cy.get('.username-input').type('sarahperez14');
-    //cy.get('.password-input').type('C0tt0nc4ndy!');
-
-    cy.get('.option-button').click({multiple: true});
+    cy.get('.sign-in-button').click();
+    */
+    cy.get('.register-button').click();
     cy.location('pathname').should('match', /\/register$/);
     //cy.contains('full-calendar').should('be.visible');
 
@@ -15,5 +17,12 @@ describe('Calendar Page Test', () => {
     cy.get('.password-input').type('sarahpassword123!');
 
     cy.get('.option-button').click();
+
+    /*
+    cy.get('.username-input').type('SarahUsername');
+    cy.get('.password-input').type('ssarahpassword123!');
+
+    cy.get('.sign-in-button').click().first();
+    */
   })
 })
