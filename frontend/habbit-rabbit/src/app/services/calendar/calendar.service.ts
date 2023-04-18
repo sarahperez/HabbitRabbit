69ee796ae68d38df44c5d03d088b434d4b914c22
Sarah Perez <perezsarah14@gmail.com>
2023-04-17 22:28:55 -0400

    calendar service stopping point w delete error

--- frontend/habbit-rabbit/src/app/services/calendar/calendar.service.ts
+++ frontend/habbit-rabbit/src/app/services/calendar/calendar.service.ts
@@ -24,7 +24,7 @@ export class CalendarService {
   ) { }
 
   addEvent(EventID: string, Start: string, End: string, Title: string): any {
-    lastValueFrom(this.http.post('http://localhost:3000/EditCal', { "user" : sessionStorage.getItem('userID'), "eventID" : EventID, "startStr" : Start, "endStr": End, "title" : Title })).then(async (res: any) => {
+    lastValueFrom(this.http.post('http://localhost:3000/EditCal', { "user" : +(sessionStorage.getItem('userID'))!, "eventID" : +EventID, "startStr" : Start, "endStr": End, "title" : Title })).then(async (res: any) => {
       if (res) {
         this.errorSubject.next(null);
       } else if (res.Message) {
@@ -34,9 +34,8 @@ export class CalendarService {
   }
 /*
   deleteEvent(EventID: string, Start: string, End: string, Title: string): any {
-    lastValueFrom(this.http.delete('http://localhost:3000/EditCal', { "user" : sessionStorage.getItem('userID'), "eventID" : EventID, "startStr" : Start, "endStr": End, "title" : Title })).then(async (res: any) => {
-      if (res&&res.jwt) {
-        sessionStorage.setItem('jwt', res.jwt);
+    lastValueFrom(this.http.delete('http://localhost:3000/EditCal', { "user" : +(sessionStorage.getItem('userID'))!, "eventID" : +EventID, "startStr" : Start, "endStr": End, "title" : Title })).then(async (res: any) => {
+      if (res) {
         this.errorSubject.next(null);
       } else if (res.Message) {
         this.errorSubject.next(res.Message);
@@ -44,9 +43,8 @@ export class CalendarService {
     });
   }
 */
-
   loadEvents(): any {
-    lastValueFrom(this.http.post('http://localhost:3000/CalStatus', { "user": sessionStorage.getItem('userID')})).then(async (res: any) => {
+    lastValueFrom(this.http.post('http://localhost:3000/CalStatus', { "user": +sessionStorage.getItem('userID')!})).then(async (res: any) => {
       if (res) {
         sessionStorage.setItem('events', res.data['items']);
       }
